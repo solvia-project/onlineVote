eb94deaacb85a8c896a5c6a40c7c0ee6c867001d
fazaram <fazaramanugraha@gmail.com>
2025-11-25 00:58:05 +0700

    update forgot password

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('register');
 });
+
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+});
