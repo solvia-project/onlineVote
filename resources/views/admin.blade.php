1daafef4c2d9a0c63f97068dc4ef9d6aa857758c
fazaram <fazaramanugraha@gmail.com>
2025-11-26 00:55:11 +0700

    update dashboard admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -147,23 +147,9 @@
         <header class="main-nav">
     <div class="sidebar-user text-center">
         <a class="setting-primary" href="javascript:void(0)"><i data-feather="settings"></i></a><img class="img-90 rounded-circle" src="https://laravel.pixelstrap.com/viho/assets/images/dashboard/1.png" alt="" />
-        <div class="badge-bottom"><span class="badge badge-primary">New</span></div>
         <a href="user-profile"> <h6 class="mt-3 f-14 f-w-600">Emay Walter</h6></a>
-        <p class="mb-0 font-roboto">Human Resources Department</p>
-        <ul>
-            <li>
-                <span><span class="counter">19.8</span>k</span>
-                <p>Follow</p>
-            </li>
-            <li>
-                <span>2 year</span>
-                <p>Experince</p>
-            </li>
-            <li>
-                <span><span class="counter">95.2</span>k</span>
-                <p>Follower</p>
-            </li>
-        </ul>
+        <p class="mb-0 font-roboto">Admin</p>
+
     </div>
     <nav>
         <div class="main-navbar">
@@ -172,11 +158,6 @@
                 <ul class="nav-menu custom-scrollbar">
                     <li class="back-btn">
                         <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
-                    </li>
-                    <li class="sidebar-main-title">
-                        <div>
-                            <h6>General</h6>
-                        </div>
                     </li>
                     <li class="dropdown">
                         <a class="nav-link menu-title active" href="javascript:void(0)"><i data-feather="home"></i><span>Dashboard</span></a>
