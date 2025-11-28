c2da4ee4d972925cabfc12654041e50dde040116
fazaram <fazaramanugraha@gmail.com>
2025-11-28 09:42:02 +0700

    update admin manage

--- resources/views/admin-manage.blade.php
+++ resources/views/admin-manage.blade.php
@@ -97,7 +97,7 @@
                             <td>19:15 2011/04/25</td>
                             <td><div class="btn btn-primary">Upcoming</div></td>
                             <td>
-                                <div class="col-3 flex">
+                                <div class="row-3">
                                     <button class="btn btn-primary">Edit</button>
                                     <button class="btn btn-dark">Deactive</button>
                                     <button class="btn btn-danger">Delete</button>
@@ -113,7 +113,7 @@
                             <td>19:15 2011/04/25</td>
                             <td><div class="btn btn-dark">Done</div></td>
                             <td>
-                                <div class="col-3 flex">
+                                <div  class="row-3">
                                     <button class="btn btn-primary">Edit</button>
                                     <button class="btn btn-dark">Deactive</button>
                                     <button class="btn btn-danger">Delete</button>
@@ -129,7 +129,7 @@
                             <td>19:15 2011/04/25</td>
                             <td><div class="btn btn-success">Active</div></td>
                             <td>
-                                <div class="col-3 flex">
+                                <div  class="row-3">
                                     <button class="btn btn-primary">Edit</button>
                                     <button class="btn btn-dark">Deactive</button>
                                     <button class="btn btn-danger">Delete</button>
@@ -145,4 +145,63 @@
             </div>
           </div>
     </div>
+
+    {{-- modals add --}}
+    <div class="container-fluid p-0">
+        <div class="row m-0">
+          <div class="col-12 p-0">
+            <div class="login-card">
+              <form class="theme-form login-form">
+                <h4>Create your account</h4>
+                <h6>Enter your personal details to create account</h6>
+                <div class="form-group">
+                  <label>Your Name</label>
+                  <div class="small-group">
+                    <div class="input-group"><span class="input-group-text"><i class="icon-user"></i></span>
+                      <input class="form-control" type="text" required="" placeholder="Fist Name">
+                    </div>
+                    <div class="input-group"><span class="input-group-text"><i class="icon-user"></i></span>
+                      <input class="form-control" type="email" required="" placeholder="Last Name">
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label>Email Address</label>
+                  <div class="input-group"><span class="input-group-text"><i class="icon-email"></i></span>
+                    <input class="form-control" type="email" required="" placeholder="Test@gmail.com">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label>Password</label>
+                  <div class="input-group"><span class="input-group-text"><i class="icon-lock"></i></span>
+                    <input class="form-control" type="password" name="login[password]" required="" placeholder="*********">
+                    <div class="show-hide"><span class="show">                         </span></div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="checkbox">
+                    <input id="checkbox1" type="checkbox">
+                    <label class="text-muted" for="checkbox1">Agree with <span>Privacy Policy                   </span></label>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <button class="btn btn-primary btn-block" type="submit">Create Account</button>
+                </div>
+                <div class="login-social-title">
+                  <h5>signup with</h5>
+                </div>
+                <div class="form-group">
+                  <ul class="login-social">
+                    <li><a href="../../login.html" target="_blank"><i data-feather="linkedin"></i></a></li>
+                    <li><a href="../../login.html" target="_blank"><i data-feather="twitter"></i></a></li>
+                    <li><a href="../../login.html" target="_blank"><i data-feather="facebook"></i></a></li>
+                    <li><a href="https://www.instagram.com/login" target="_blank"><i data-feather="instagram">                  </i></a></li>
+                  </ul>
+                </div>
+                <p>Already have an account?<a class="ms-2" href="log-in.html">Sign in</a></p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
