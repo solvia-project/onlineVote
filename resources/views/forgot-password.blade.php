39e3367a2d557672a528f054395d3a43a82269e6
fazaram <fazaramanugraha@gmail.com>
2025-12-04 23:29:21 +0700

    rapih rapih

--- resources/views/forgot-password.blade.php
+++ resources/views/forgot-password.blade.php
@@ -1,72 +1,130 @@
 @extends('layouts.app')
 
 @section('title', 'Forgot Password')
+
 @section('content')
-<!-- Loader starts-->
+
+<!-- Loader Start -->
 <div class="loader-wrapper">
     <div class="theme-loader">
         <div class="loader-p"></div>
     </div>
 </div>
-<!-- Loader ends-->
-<!-- page-wrapper Start-->
-<section>
+<!-- Loader End -->
+
+<section class="auth-section">
     <div class="container-fluid p-0">
-        <div class="row m-0">
-            <div class="col-12 p-0">
+        <div class="row justify-content-center align-items-center" style="min-height:100vh;">
+            <div class="col-12">
+
                 <div class="login-card">
                     <div class="login-main">
-                        <form class="theme-form login-form">
+
+                        <form class="theme-form login-form" method="POST" action="#">
+                            @csrf
+
                             <h4 class="mb-3">Forgot Password</h4>
+
+                            {{-- Email --}}
                             <div class="form-group">
                                 <label>Email Address</label>
                                 <div class="input-group">
-                                    <span class="input-group-text"><img src="{{ asset('images/login/email.png') }}" alt=""></i></span>
-                                    <input type="email" class="form-control" required placeholder="email@mail.com">
+                                    <span class="input-group-text">
+                                        <img src="{{ asset('images/login/email.png') }}" alt="">
+                                    </span>
+                                    <input type="email"
+                                           name="email"
+                                           class="form-control"
+                                           required
+                                           placeholder="email@mail.com">
                                 </div>
                             </div>
+
+                            {{-- New Password --}}
                             <div class="form-group">
                                 <label>New Password</label>
-                                <div class="input-group"><span class="input-group-text"><i class="icon-lock"></i></span>
-                                    <input class="form-control" type="password" name="login[password]" required="" placeholder="*********">
-                                    <div class="show-hide"><span class="show"></span></div>
+                                <div class="input-group">
+
+                                    <input type="password"
+                                           name="new_password"
+                                           id="new_password"
+                                           class="form-control"
+                                           required
+                                           placeholder="********">
+
+                                    <span class="input-group-text toggle-password"
+                                          data-target="#new_password"
+                                          style="cursor:pointer; background:white;">
+                                        <img src="{{ asset('images/login/eye.png') }}" class="icon-eye" alt="show">
+                                    </span>
                                 </div>
                             </div>
+
+                            {{-- Retype Password --}}
                             <div class="form-group">
                                 <label>Retype Password</label>
-                                <div class="input-group"><span class="input-group-text"><i class="icon-lock"></i></span>
-                                    <input class="form-control" type="password" name="login[password]" required="" placeholder="*********">
+                                <div class="input-group">
+
+                                    <input type="password"
+                                           name="confirm_password"
+                                           id="confirm_password"
+                                           class="form-control"
+                                           required
+                                           placeholder="********">
+
+                                    <span class="input-group-text toggle-password"
+                                          data-target="#confirm_password"
+                                          style="cursor:pointer; background:white;">
+                                        <img src="{{ asset('images/login/eye.png') }}" class="icon-eye" alt="show">
+                                    </span>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <button class="btn btn-primary btn-block" type="submit">Reset Password &rightarrow;</button>
+
+                            <div class="form-group mt-3">
+                                <button class="btn btn-primary btn-block" type="submit">
+                                    Reset Password →
+                                </button>
                             </div>
 
                         </form>
+
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
 </section>
-<!-- page-wrapper end-->
-<!-- latest jquery-->
-<script src="js/jquery-3.5.1.min.js"></script>
-<!-- feather icon js-->
-<script src="js/icons/feather-icon/feather.min.js"></script>
-<script src="js/icons/feather-icon/feather-icon.js"></script>
-<!-- Sidebar jquery-->
-<script src="js/sidebar-menu.js"></script>
-<script src="js/config.js"></script>
-<!-- Bootstrap js-->
-<script src="js/bootstrap/popper.min.js"></script>
-<script src="js/bootstrap/bootstrap.min.js"></script>
-<!-- Plugins JS start-->
-<script src="js/sweet-alert/sweetalert.min.js"></script>
-<!-- Plugins JS Ends-->
-<!-- Theme js-->
-<script src="js/script.js"></script>
-<!-- login js-->
-<!-- Plugin used-->
-</body>
+
+{{-- Password Toggle Script --}}
+<script>
+    document.querySelectorAll('.toggle-password').forEach(btn => {
+        btn.addEventListener('click', function () {
+            let input = document.querySelector(this.dataset.target);
+            let icon = this.querySelector('.icon-eye');
+
+            if (input.type === "password") {
+                input.type = "text";
+            } else {
+                input.type = "password";
+            }
+        });
+    });
+</script>
+
+@push('styles')
+<style>
+    #app {
+        margin-top: 0 !important;
+        margin-bottom: 0 !important;
+    }
+    .auth-section {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+@endpush
+
 @endsection
